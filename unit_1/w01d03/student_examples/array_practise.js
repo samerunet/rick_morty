fa3247a29f4b9eb8e4ebcee46d952af33e627744
Sami John <samijohn@Samis-MacBook-Pro.local>
2022-07-13 12:22:46 -0700

    lessons

---
+++ unit_1/w01d03/student_examples/array_practise.js
@@ -0,0 +1,57 @@
+// const arr = [];
+// console.log(arr);
+
+// console.log(satchel[0]);
+
+// const faveFoods = ["swiss mushroom burger", "lamb chops", "california burrito"];
+
+// console.log(faveFoods);
+
+// satchel[2] = "glowing orb";
+// console.log(satchel);
+// console.log(satchel[satchel.length / 2])
+
+// const shortArray = ["first", "second", "third"];
+
+// console.log(shortArray[Math.floor(shortArray.length / 2)]);
+
+const satchel = [
+	"yurt",
+	"mattress",
+	"candles",
+	"map",
+	"glasses",
+	"rupees",
+	"pepto",
+	"boomerang",
+];
+
+const squares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+
+// for (let i = 0; i < satchel.length; i++) {
+// 	console.log(satchel[i]);
+// }
+
+// for (let i = 0; i < satchel.length; i++) {
+// 	if (i % 2 === 0) {
+// 		console.log(`index: ${i}`);
+// 		console.log(`element: ${satchel[i]}`);
+// 	}
+// }
+
+// for (let i = 0; i < satchel.length; i++) {
+// 	if (i % 2 === 0 || i === 5) {
+// 		console.log(`index: ${i}`);
+// 		console.log(`element: ${satchel[i]}`);
+// 	}
+// }
+
+// const pairs = [
+// 	["Harold", "Kumar"], // 0
+// 	["Laurel", "Hardy"], //1
+// ];
+
+// pairs[0][1] = "Maude";
+// console.log(pairs[0][1]);
+
+
