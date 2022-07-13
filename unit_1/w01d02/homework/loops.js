f876759b3aabf28f6c409319c6fa07b30b6935be
Sami John <samijohn@Samis-MacBook-Pro.local>
2022-07-12 21:12:58 -0700

    homework done

---
+++ unit_1/w01d02/homework/loops.js
@@ -0,0 +1,32 @@
+// Create a loop that logs the numbers from 0-99 (ascending)
+
+for (let i = 0; i <= 99; i++) {
+	console.log(i);
+}
+
+// Create a loop that logs the numbers from 99-0 (descending)
+
+for (let i = 99; i >= 0; i--) {
+	console.log(i);
+}
+
+// Create a loop that logs the EVEN numbers from 0-98 (ascending)
+for (let i = 0; i <= 98; i+=2) {
+	console.log(i);
+}
+
+// Create a loop that logs the EVEN numbers from 98-0 (descending)
+for (let i = 98; i >= 0; i-=2) {
+	console.log(i);
+}
+
+// Create a loop that logs the ODD numbers from 0-99 (ascending)
+for (let i = 0; i <= 99; i+=3) {
+	console.log(i);
+}
+
+// Create a loop that logs the ODD numbers from 99-0 (descending)
+
+for (let i = 99; i >= 0; i-=3) {
+	console.log(i);
+}
