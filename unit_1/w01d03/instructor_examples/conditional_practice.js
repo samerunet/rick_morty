7baf8e600b4a6af7fc8d201fb912451001fbcf58
qui1217 <qui1217@gmail.com>
2022-07-13 13:48:23 -0600

    pushing examples

---
+++ unit_1/w01d03/instructor_examples/conditional_practice.js
@@ -0,0 +1,50 @@
+// let isMMColorGreen = true
+//
+// if (isMMColorGreen) {
+//   console.log('Ew gross!')
+// } else {
+//   console.log('yum!')
+// }
+
+
+// const isPartyTonight = true
+// const miniGolfCoupon = true
+// const saleAtTheMall = true
+//
+// if (isPartyTonight) {
+//   console.log('Wooo Party!')
+// } else if (miniGolfCoupon) {
+//   console.log('Hole in one!')
+// } else if (saleAtTheMall) {
+//   console.log('Buy more save more!')
+// } else {
+//   console.log('I love baking cookies')
+// }
+
+// let mmColor = 'green'
+//
+// if (mmColor === 'green') {
+//   console.log('ew!')
+// } else {
+//   console.log('yum!')
+// }
+
+// if (5 % 2 === 0) {
+//   console.log('num is even')
+// }
+
+// if (5 % 2 !== 0) {
+//   console.log('num is odd')
+// }
+//
+// if (49 % 7 === 0) {
+//   console.log('num is divisible by 7')
+// }
+
+let num = 18
+
+if (num % 2 === 0) {
+  console.log('num is even')
+} else {
+  console.log('num is odd')
+}
