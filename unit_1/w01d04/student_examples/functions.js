990460f47cfaedbd0359716c2794d224d4eb2721
samerunet <samerunet@gmail.com>
2022-07-14 11:23:31 -0700

    pull excersize

--- unit_1/w01d04/student_examples/functions.js
+++ unit_1/w01d04/student_examples/functions.js
@@ -1,16 +1,65 @@
-const printBanner = () => {
-	console.log("========================");
-	console.log("Insert Banner Text Here");
-	console.log("========================");
-};
+// const printBanner = () => {
+// 	console.log("========================");
+// 	console.log("Insert Banner Text Here");
+// 	console.log("========================");
+// };
+
+// printBanner();
+
+// const printSummary = () => {
+//     console.log(10 + 10);
+// }
+
+// printSummary();
+
+// const printTriangle = () => {
+//     console.log("#")
+//     console.log("##")
+//     console.log("###")
+//     console.log("####")
+
+// }
+
+// printTriangle();
+
+// const checkInputLength = input => {
+//     if (input.length > 10) {
+//       console.log('input length is greater than 10. Please shorten!')
+//     } else {
+//       console.log('input length is not greater than 10. Good to go!')
+//     }
+//   }
+
+//                 // (parameter)
+// const sayName = (name) => {
+//     console.log(`hello my name is ${name}`)
+// }
+
+// // arguments
+// sayName('potato')
+
+// const calculateArea = (num1, num2) => {
+// 	console.log(num1 * num2);
+// };
+
+// calculateArea(1, 2); // any data type or structure
+
+// const minusOne = (num1) => {
+//         console.log(num1 - 1);
+// }
+
+// minusOne(5)
+
+// const calculateArea = (num1, num2) => {
+// 	return num1 - num2;
+// };
+// console.log(calculateArea(calculateArea(2, 3), calculateArea(2, 4)));
 
 
-printBanner();
-
-
-const printSummary = () => {
-    console.log(10 + 10);
-}
-
-printSummary();
-
+// const example = input => {
+//     if (input == 'none') {
+//       return 0
+//     }
+//     return 1
+//   }
+  
