663d03a6388887bfa9d9d05be0f88029bf8ed25b
samerunet <samerunet@gmail.com>
2022-07-14 13:17:03 -0700

    scope

---
+++ unit_1/w01d04/student_examples/scope.js
@@ -0,0 +1,71 @@
+// const exampleFunction = () => {
+// 	// num is locally scoped to example function
+// 	const num = 100;
+// 	if (true) {
+// 		console.log(num * num);
+// 	}
+// };
+// console.log(num); // num is not available outside the function
+
+// const item = "meatballs";
+
+// const exampleFunction = () => {
+// 	console.log(`${item} within function `);
+// };
+
+// exampleFunction();
+
+// const setItem = () => {
+//     const item = "meatballs";
+//     return item;
+// }
+
+// const getItem = () => {
+//     return item
+// }
+
+// const returnName = {} => {
+//     return "Brendan"
+// }
+
+// const returnGreeting = {} => {
+//     return `oh hey ${returnName()}`
+// }
+
+// // console.log(returnGreeting())
+
+// for (let i = 0; i < 100; i++) {
+//     console.log('Inside the block: ', i)
+//   }
+
+//   console.log('Outside the block: ', i)
+
+// for (i = 0; i < 10; i++) {
+// 	console.log(i);
+// }
+
+// console.log(i);
+
+
+// const age = 21
+// let message = ''
+
+// if (age < 21) {
+//   message = 'You cannot buy the beer'
+// } else {
+//   message = 'You can buy the beer'
+// }
+
+// console.log(message)
+
+
+// const words = "that's a..."
+// {
+//   const item = 'spicy meatball'
+//   const start = 'mama mia!'
+//   {
+//     console.log(start)
+//     console.log(words)
+//     console.log(item)
+//   }
+// }
