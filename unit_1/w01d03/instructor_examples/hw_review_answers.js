df6da5d5fed5ae6656aabbcec5ab4ef8ac534e87
Jesse Maretz <jesse.maretz@gmail.com>
2022-07-14 14:41:01 -0400

    pushing w01d04 hw

---
+++ unit_1/w01d03/instructor_examples/hw_review_answers.js
@@ -0,0 +1,84 @@
+//======EASY GOING=========
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i)
+// }
+
+//======GET EVEN===========
+// for (let i = 0; i <= 200; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+//======FIZZ BUZZ==========
+// for (let i = 1; i <= 100; i++) {
+//     if (i% 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz")
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz")
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz")
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+
+
+//======WILD WILD LIFE=====
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+
+
+// plantee[2] += 1
+// console.log(plantee[2])
+
+// console.log(wolfy[3])
+// wolfy[3] = "Gotham City"
+// console.log(wolfy[3])
+// wolfy.splice(3, 1, "Gotham City")
+// console.log(wolfy)
+
+//======NINJA TURTLES======
+// const turtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+// turtles.reverse()
+
+// for (names of turtles) {
+//     console.log(names.toUpperCase())
+// }
+
+//======MOVIES + METHODS===
+
+
+//======WHERES WALDO=======
+const whereIsWaldo = [
+    ["Timmy", "Frank"], 
+    "Eggbert",
+    ["Lucinda", "Jacc", "Neff", "Snoop"],
+    ["Petunia", ["Baked Goods", "Waldo"]]
+]
+
+// whereIsWaldo.splice(1, 1)
+// console.log(whereIsWaldo)
+
+// whereIsWaldo[2].splice(2, 1, "No One")
+// console.log(whereIsWaldo)
+
+console.log(whereIsWaldo[3][1][1])
+
+//======EXCITED KITTEN=====
+
+// const catQuotes = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+
+// for (let i = 0; i < 20; i++) {
+//     console.log("Love me, pet me! HSSSSSS!")
+//     if (i % 2 === 0) {
+//         console.log(catQuotes[Math.floor(Math.random()*3)])
+//     }
+// }
+
+//======FIND THE MEDIAN====
+
