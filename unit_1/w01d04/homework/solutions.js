e968332f1374a8b0451ff379a87d01c8d3d05e96
samerunet <samerunet@gmail.com>
2022-07-14 15:14:31 -0700

    "sum array completed"

--- unit_1/w01d04/homework/solutions.js
+++ unit_1/w01d04/homework/solutions.js
@@ -1,38 +1,64 @@
 // verbal qs
+//
+// 1. What is the difference between a **parameter** and an **argument**?
+// 2. Within a function, what is the difference between **return** and **console.log**?
+
+// 1 parameter is defined in the function as a name inside ()
+// argument is defined as the actual value and assigned to parameter
+
+// 2.  in the function, return is assigned to return value,  when the console.log
+// only prints the outcome and does not store it
+
+// palindrome
+
+// creat function
+// asign parameter
+// uppercase parameter
+// split parameter
+// reverse parameter
+// compare within if statement if every letter of the word mutches
+// return true or false
+// console.log result
+
+// const checkPalindrome = (word) => {
+// 	word = word.toUpperCase();
+// 	let wordMutate = word.toUpperCase().split("").reverse().join("");
+
+// 	// return word === wordMutate ? true : false; // another way of using the if statement
+// 	if (word === wordMutate) {
+// 		return true;
+// 	} else if (word !== wordMutate) {
+// 		return false;
+// 	}
+// };
+
+// console.log(checkPalindrome("potato"));
+// console.log(checkPalindrome("RaceCar"));
+
+// sum array
+
+// const sumArray = (numbers) => {
+//     let sum = 0
+//     for (let i = 0; i < numbers.length; i++) {
+//         sum += numbers[i];
+//         console.log(i)
+//     }  
+//     return sum
+// }
+
+//  console.log(sumArray([1, 2, 3, 4, 5, 6]))
 
 
-// 1. here's my answer to 1 
 
+// console.log(sum);
+// use loop
 
-// palindrome 
-
-const checkPalindrome (word) => {
-    
-        word = (word.toUpperCase())
-
-    let  wordReverse = word.split("").reverse().join("")
-    
-    console.log(wordReverse)
-} 
-
-
-
-
-
-// sum array 
-
-// use loop 
-
-
-//prime numbers 
+//prime numbers
 
 // a number that evenly divides by a number and itself
 
+// rock paper scissors
 
-// rock paper scissors 
+// 2 funsctions cp move , user move
 
-
-// 2 funsctions cp move , user move 
-
-// create if statement to check who won 
-
+// create if statement to check who won
