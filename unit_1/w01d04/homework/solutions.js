c86c3f220ecb71e059bb22aaebce54f75b15354b
samerunet <samerunet@gmail.com>
2022-07-14 13:32:11 -0700

    scope

---
+++ unit_1/w01d04/homework/solutions.js
@@ -0,0 +1,25 @@
+// verbal qs
+
+
+// 1. here's my answer to 1 
+
+
+// palindrome 
+
+
+// sum array 
+// use loop 
+
+
+//prime numbers 
+
+// a number that evenly divides by a number and itself
+
+
+// rock paper scissors 
+
+
+// 2 funsctions cp move , user move 
+
+// create if statement to check who won 
+
