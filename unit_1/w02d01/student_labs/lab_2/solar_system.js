656b5e97978216f73a52e7e6db0303e3a1c3543d
samerunet <samerunet@gmail.com>
2022-07-18 11:57:07 -0700

    solar system complete

---
+++ unit_1/w02d01/student_labs/lab_2/solar_system.js
@@ -0,0 +1,49 @@
+// const solarSystem = [
+// 	{ name: "Mercury", ringSystem: false, moons: [] },
+// 	{ name: "Venus", ringSystem: false, moons: [] },
+// 	{ name: "Earth", ringSystem: false, moons: ["The Moon"] },
+// 	{ name: "Mars", ringSystem: false, moons: ["Phobos", "Deimos"] },
+// 	{
+// 		name: "Jupiter",
+// 		ringSystem: true,
+// 		moons: ["Europa", "Ganymede", "Io", "Callisto"],
+// 	},
+// 	{
+// 		name: "Saturn",
+// 		ringSystem: true,
+// 		moons: ["Titan", "Enceladus", "Rhea", "Mimas"],
+// 	},
+// 	{
+// 		name: "Uranus",
+// 		ringSystem: true,
+// 		moons: ["Miranda", "Titania", "Ariel", "Umbriel"],
+// 	},
+// 	{ name: "Neptune", ringSystem: true, moons: ["Triton", "Nereid"] },
+// ];
+
+// console.log(solarSystem[0].ringSystem);
+
+// console.log(solarSystem[4])
+
+// solarSystem[1].moons = ["Endor"]
+
+
+// console.log(
+//     solarSystem[1])
+
+// solarSystem.push({ name: "Pluto", ringSystem: true, moon: ["Charon"] })
+
+// console.log(solarSystem)
+
+// solarSystem[2].diameter = 7917
+
+// solarSystem[0].ringSystem = true
+
+// solarSystem[6].moons[3] = "Oberon"
+
+// for(let i = 0; i < solarSystem.length; i++){
+//     if(solarSystem[i].ringSystem === true){
+//       console.log(solarSystem[i])
+//     }
+// }
+  
