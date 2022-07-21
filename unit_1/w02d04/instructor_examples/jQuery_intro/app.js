5be3c8f660a216939233902d8f5ad00ce8e55e4c
qui1217 <qui1217@gmail.com>
2022-07-21 13:49:15 -0600

    pushing examples

--- unit_1/w02d04/instructor_examples/jQuery_intro/app.js
+++ unit_1/w02d04/instructor_examples/jQuery_intro/app.js
@@ -1,3 +1,37 @@
-if (typeof $ == 'undefined'){
-  console.log('oops! I still have to link my jQuery properly!');
-} else {console.log('I did it! I linked jQuery and this js file properly!')};
+$(() => {
+  //create
+  const $div = $('<div>')
+  const $h3 = $('<h3>')
+  const $div2 = $('<div>')
+  const $h32 = $('<h3>')
+  const $image = $('<img>')
+
+
+
+  //editing/changing
+  $div.addClass('meat')
+  $h3.text('Ostrich')
+  $div2.addClass('vegetable')
+  $h32.text('carrot')
+  $image.attr('alt', 'big cheese')
+  $image.attr('src', 'https://images.unsplash.com/photo-1452195100486-9cc805987862?auto=format&fit=crop&w=750&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D')
+
+
+  //appending
+  $div.append($h3)
+  $div2.append($h32)
+  $('body').prepend($div2)
+  $('body').append($div)
+  $('body').prepend($image)
+
+  const $divs = $('div')
+  $divs.eq(2).children().text('Kohlrabi')
+  // let $meatDivs = $('.meat')
+  // $meatDivs.remove()
+  // $divs.hide()
+
+  const $newH3 = $('<h3>').text('wine')
+  const $secondDiv = $divs.eq(1)
+  $secondDiv.empty()
+  $secondDiv.append($newH3)
+})
