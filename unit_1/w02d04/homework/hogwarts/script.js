87ec797ec7cac168c157ed1a563f53acdb92b427
samerunet <samerunet@gmail.com>
2022-07-21 22:25:02 -0700

    Commit 3 - I added list elements

--- unit_1/w02d04/homework/hogwarts/script.js
+++ unit_1/w02d04/homework/hogwarts/script.js
@@ -16,4 +16,19 @@ $(() => {
 	$h4v2.addClass("wand");
 
 	$container.append($h2, $h3, $h4, $h4v2);
+
+	let $ul = $("<ul>");
+
+	$ul.addClass("storage");
+
+	let $li = $("<li>").text("butter beer");
+	let $li2 = $("<li>").text("invisible cloak").addClass("secret");
+	let $li3 = $("<li>").text("magic map").addClass("secret");
+	let $li4 = $("<li>").text("time turner").addClass("secret");
+	let $li5 = $("<li>").text("leash").addClass("pet");
+	let $li6 = $("<li>").text("Bertie Bott's Every Flavor [Jelly] Beans");
+
+	$ul.append($li, $li2, $li3, $li4, $li5, $li6);
+
+	$container.append($ul);
 });
