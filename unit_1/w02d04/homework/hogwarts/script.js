329521aab242f0f83aabf67e4e71b66a7a2ad511
samerunet <samerunet@gmail.com>
2022-07-21 22:51:18 -0700

    Commit 4

--- unit_1/w02d04/homework/hogwarts/script.js
+++ unit_1/w02d04/homework/hogwarts/script.js
@@ -30,5 +30,51 @@ $(() => {
 
 	$ul.append($li, $li2, $li3, $li4, $li5, $li6);
 
-	$container.append($ul);
+	$container.prepend($ul);
+	let $table = $("<table>");
+	let $thead1 = $("<thead>");
+	let $thead1th1 = $("<th>");
+	let $thead1th2 = $("<th>");
+	let $tr1 = $("<tr>");
+	let $td1 = $("<td>");
+	let $td2 = $("<td>");
+	let $tr2 = $("<tr>");
+	let $td3 = $("<td>");
+	let $td4 = $("<td>");
+	let $tr3 = $("<tr>");
+	let $td5 = $("<td>");
+	let $td6 = $("<td>");
+	let $tr4 = $("<tr>");
+	let $td7 = $("<td>");
+	let $td8 = $("<td>");
+	let $tr5 = $("<tr>");
+	let $td9 = $("<td>");
+	let $td10 = $("<td>");
+	let $tr6 = $("<tr>");
+	let $td11 = $("<td>");
+	let $td12 = $("<td>");
+	let $tr7 = $("<tr>");
+	let $td13 = $("<td>");
+	let $td14 = $("<td>");
+
+	$table.append($thead1);
+	$thead1.append($thead1th1.text("Day"), $thead1th2.text("Classes"));
+
+	$tr1.append($td1.text("Monday"), $td2.text("Herbology"));
+
+	$tr2.append($td3.text("Tuesday"), $td4.text("Charms"));
+
+	$tr3.append($td5.text("Wednesday"), $td6.text("Potions"));
+
+	$tr4.append($td7.text("Thursday"), $td8.text("Monsters"));
+
+	$tr5.append($td9.text("Friday"), $td10.text("Mugglescript"));
+
+	$tr6.append($td11.text("Saturday"), $td12.text("Transfiguration"));
+
+	$tr7.append($td13.text("Sunday"), $td14.text("Healing"));
+
+	$table.append($tr1, $tr2, $tr3, $tr4, $tr5, $tr6, $tr7);
+	$table.addClass("table");
+	$container.append($table);
 });
