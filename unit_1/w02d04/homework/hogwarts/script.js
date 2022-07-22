35518c83ace5364e52bd4dfa9229bb76df848a16
samerunet <samerunet@gmail.com>
2022-07-21 22:13:16 -0700

    My power grows with my dedication and hard work

--- unit_1/w02d04/homework/hogwarts/script.js
+++ unit_1/w02d04/homework/hogwarts/script.js
@@ -4,4 +4,16 @@ $(() => {
 	const $h1 = $("<h1>").text("Hogwarts");
 	$h1.addClass("headTitle");
 	$container.append($h1);
+
+	let $h2 = $("<h2>").text("Sami Sipahi");
+	let $h3 = $("<h3>").text("Hufflepuff");
+	let $h4 = $("<h4>").text("OWL");
+	let $h4v2 = $("<h4>").text("Holly Wand with Unicorn Hair Core");
+
+	$h2.addClass("name");
+	$h3.addClass("house");
+	$h4.addClass("pet");
+	$h4v2.addClass("wand");
+
+	$container.append($h2, $h3, $h4, $h4v2);
 });
