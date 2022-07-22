b5b5191c9d83e722c7162c0d2f9e50152296063c
samerunet <samerunet@gmail.com>
2022-07-21 19:36:15 -0700

    I added my first element and it was fun

--- unit_1/w02d04/homework/hogwarts/script.js
+++ unit_1/w02d04/homework/hogwarts/script.js
@@ -1,4 +1,7 @@
 $(() => {
-    const $container = $("#container");
-    
+	const $container = $("#container");
+	console.log($container);
+	const $h1 = $("<h1>").text("Hogwarts");
+	$h1.addClass("headTitle");
+	$container.append($h1);
 });
