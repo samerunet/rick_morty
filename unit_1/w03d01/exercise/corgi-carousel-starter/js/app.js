740df430226096b4bb353463d4c539ed0186f29e
samerunet <samerunet@gmail.com>
2022-07-25 14:58:56 -0700

    css

--- unit_1/w03d01/exercise/corgi-carousel-starter/js/app.js
+++ unit_1/w03d01/exercise/corgi-carousel-starter/js/app.js
@@ -1,12 +1,17 @@
 // console.log("corgi carousel connected to app.js!");
 $(() => {
+	let currentImageIndex = 0;
+	$("next").on("click", () => {
+		$(".carousel-images")
+			.children()
+			.eq(currentImageIndex)
+			.css("display", "none");
+		currentImageIndex++;
+		$(".carousel-images")
+			.children()
+			.eq(currentImageIndex)
+			.css("display", "block");
 
-
-
-
-
-
-
-
-
+		currentImageIndex;
+	});
 });
