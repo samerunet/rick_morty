786ecca5dbb28cbaf58ff678f14ac40bebbae53e
samerunet <samerunet@gmail.com>
2022-08-23 06:11:35 -0700

    homework

---
+++ unit_3/w07d01/student_labs/lab_1/jservice/src/App.js
@@ -0,0 +1,52 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Buttons from "./components/Buttons";
+
+function App() {
+	const [score, setScore] = useState(0);
+	let [characters, setCharacters] = useState([]);
+	let [answers, setAnswers] = useState([]);
+
+	let gameUp = () => {
+		setScore(score + 1);
+	};
+	let gameDown = () => {
+		setScore(score + 1);
+	};
+	const getCharacters = () => {
+		axios.get("http://jservice.io/api/random").then((response) => {
+			setCharacters(response.data[0]);
+			// console.log(response.data[0]);
+		});
+	};
+	useEffect(() => {
+		getCharacters();
+	}, []);
+	return (
+		<div className='container'>
+			<h1>WELCOME TO JEOPARDY!</h1>
+			<div className='score'>
+				<h1>Score</h1>
+				<Buttons />
+			</div>
+			<h1>Let's Play</h1>
+			<button className='button' onClick={getCharacters}>
+				Get Question
+			</button>
+			<div>Category: </div>
+			<div>
+				<h1>Points: </h1>
+			</div>
+			<div>
+				<h1>Answer: </h1>
+			</div>
+			<div>
+				<h1>{characters.question}</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ unit_3/w07d01/student_labs/lab_1/jservice/src/components/Buttons.js
@@ -0,0 +1,11 @@
+const Buttons = () => {
+	return (
+		<div className='buttons'>
+			<button type='button'>Increase</button>
+			<button type='button'>Decrease</button>
+			<button type='button'>Reset</button>
+		</div>
+	);
+};
+
+export default Buttons;
