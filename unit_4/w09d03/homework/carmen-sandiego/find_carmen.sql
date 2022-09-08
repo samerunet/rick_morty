45c6b018d8bfe97793d551456d7bcdee9f2734cc
samerunet <samerunet@gmail.com>
2022-09-07 21:45:21 -0700

    step 1

--- unit_4/w09d03/homework/carmen-sandiego/find_carmen.sql
+++ unit_4/w09d03/homework/carmen-sandiego/find_carmen.sql
@@ -25,10 +25,17 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+--select * from country where region = 'Southern Europe' order by population desc;
+-- VAT  | Holy See (Vatican City State) 
+-- | Europe    | Southern Europe |         0.4 |      1929 |       1000 |                |       9.00 |            | Santa Sede/Citt� del Vaticano | Independent Church State      | Johannes Paavali II  |    3538 | VA
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
-
+-- carmen=# select * from countrylanguage where countrycode = 'VAT';
+--  countrycode | language | isofficial | percentage 
+-- -------------+----------+------------+------------
+--  VAT         | Italian  | t          |          0
+-- (1 row)
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 
