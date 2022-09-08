4c0c6a0c3fe285036d0b7c4e9e9d36371dae1ee8
samerunet <samerunet@gmail.com>
2022-09-07 21:51:53 -0700

    step 5

--- unit_4/w09d03/homework/carmen-sandiego/find_carmen.sql
+++ unit_4/w09d03/homework/carmen-sandiego/find_carmen.sql
@@ -80,6 +80,14 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+-- carmen=#  select * from city where name like '%Serra%' ;
+--   id  |         name         | countrycode |     district      | population 
+-- ------+----------------------+-------------+-------------------+------------
+--   265 | Serra                | BRA         | Esp�rito Santo  |     302666
+--   310 | Tabo�o da Serra    | BRA         | S�o Paulo       |     197550
+--   370 | Itapecerica da Serra | BRA         | S�o Paulo       |     126672
+--  3170 | Serravalle           | SMR         | Serravalle/Dogano |       4802
+-- (4 rows)
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
